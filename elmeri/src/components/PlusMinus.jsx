4434c6641440baa30bafe89c86e783af19fccc92
Arttu Uusitalo <t9uuar00@students.oamk.fi>
2023-10-13 12:18:12 +0300

    Tarkastuskohteiden lisäilyä

--- elmeri/src/components/PlusMinus.jsx
+++ elmeri/src/components/PlusMinus.jsx
@@ -1,46 +1,56 @@
 import React, { useState } from 'react';
+import './style.css';
 
 const PlusMinus = () => {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
-
-  const handleIncrement1 = () => {
-    setCount1((prevCount) => (prevCount + 1) % 100);
-  };
-
-  const handleDecrement1 = () => {
-    setCount1((prevCount) => (prevCount - 1 + 100) % 100);
-  };
-
-  const handleIncrement2 = () => {
-    setCount2((prevCount) => (prevCount + 1) % 100);
-  };
-
-  const handleDecrement2 = () => {
-    setCount2((prevCount) => (prevCount - 1 + 100) % 100);
-  };
-
   return (
-    <div>
-      <div className="component">
-        <div className="text">Kunnossa</div>
-        <div className="counter">
-          <button onClick={handleDecrement1}>-</button>
-          <span id="count1">{count1}</span>
-          <button onClick={handleIncrement1}>+</button>
-        </div>
-      </div>
-
-      <div className="component">
-        <div className="text">Ei kunnossa</div>
-        <div className="counter">
-          <button onClick={handleDecrement2}>-</button>
-          <span id="count2">{count2}</span>
-          <button onClick={handleIncrement2}>+</button>
-        </div>
-      </div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      {obsObjects.map((object, index) => (
+        <ObjectComponent key={index} object={object} index={index} />
+      ))}
     </div>
   );
 };
 
-  export default PlusMinus
+  const obsObjects =  [{target: 'Työskentely', objs: [{obj: 'Riskinotto, Suojaimet, Vaatetus'}]},
+  {target: 'Ergonomia', objs: [{obj: 'Fyysinen kuormitus'}, {obj: 'Työpisteiden ja välineiden ergonomia'}]}, 
+  {target: 'Kone- ja laiteturvallisuus', objs: [{obj: 'Koneiden kunto ja suojalaitteet'}, {obj: 'Koneiden hallintalaitteet ja merkintä'}]}, 
+  {target: 'Liikkumisturvallisuus', objs: [{obj: 'Kulkuteiden ja lattian rakenne, putoamissuojaus'}, {obj: 'Poistumistiet'}]},
+  {target: 'Järjestys', objs: [{obj: 'Kulkuteiden ja lattioiden järjestys'}, {obj: 'Pöydät, päällyset, hyllyt'}, {obj: 'Jäteastiat'}]},
+  {target: 'Työympäristötekijät', objs: [{obj: 'Melu', obj:'Valaistus'}, {obj: 'Lämpöolosuhteet'}, {obj: 'Ilman puhtaus ja käsiteltävät aineet'}]}
+];
+
+
+const ObjectComponent = ({ object, index }) => {
+  const [counts, setCounts] = useState(new Array(object.objs.length).fill(0));
+
+  const handleIncrement = (innerIndex) => {
+    const newCounts = [...counts];
+    newCounts[innerIndex] += 1;
+    setCounts(newCounts);
+  };
+
+  const handleDecrement = (innerIndex) => {
+    if (counts[innerIndex] > 0) {
+      const newCounts = [...counts];
+      newCounts[innerIndex] -= 1;
+      setCounts(newCounts);
+    }
+  };
+
+  return (
+    <div className="component" style={{ marginBottom: '20px' }}>
+      <div className="text" style={{ fontWeight: 'bold' }}>{object.target}</div>
+      {object.objs.map((item, innerIndex) => (
+        <div key={innerIndex}>
+          <div className="subText">{item.obj}</div>
+          <div className="counter">
+            <button onClick={() => handleDecrement(innerIndex)} className="button">-</button>
+            <span id={`count-${index}-${innerIndex}`}>{counts[innerIndex]}</span>
+            <button onClick={() => handleIncrement(innerIndex)} className="button">+</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default PlusMinus
--- elmeri/src/components/Tarkastuskohdat.jsx
+++ elmeri/src/components/Tarkastuskohdat.jsx
@@ -4,7 +4,7 @@ import PlusMinus from './PlusMinus'
 export default function Tarkastuskohdat() {
   return (
     <div>
-    <header style={{ position: 'absolute', left: '10px', top: '10px' }}>Elmeri</header>
+    <header style={{ position: 'absolute', left: '20px', top: '10px' }}><h1>Elmeri</h1></header>
       <div style={{ textAlign: 'center', marginTop: '50px' }}>
         <h1>Turvallisuusraportti</h1>
         <h2>Havainnoistijat:</h2>

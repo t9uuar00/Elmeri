5c928a69f0a83d24d15d93b67c4157ad1f8fc03a
Saana Pullola <t0pusa00@students.oamk.fi>
2023-10-19 00:52:30 +0300

    firestore yhdistetty

--- elmeri/src/App.js
+++ elmeri/src/App.js
@@ -2,9 +2,28 @@ import './App.css';
 import Etusivu from './components/Etusivu';
 import RaporttiHistoria from './components/RaporttiHistoria';
 
+//test
+import { collection, addDoc } from "firebase/firestore";
+import {firestoreDb} from './firebase';
+
+
+//example, for testing firestore
+const addData = async () => {
+ 
+  try {
+      const docRef = await addDoc(collection(firestoreDb, "raports"), {
+        raport: "test",    
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+}
+
 function App() {
   return (
     <div className="App">
+      <button onClick={addData}>Add data</button>
       <RaporttiHistoria/>
     </div>
   );

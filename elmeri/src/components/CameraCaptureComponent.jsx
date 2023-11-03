7beeba20816b0279d5332a166bafaf0d9b1be05f
Arttu Uusitalo <t9uuar00@students.oamk.fi>
2023-11-03 11:55:23 +0200

    Kuvanottoa päivitetty
    Kuvanottoa taas päivitetty toimivampaan

--- elmeri/src/components/CameraCaptureComponent.jsx
+++ elmeri/src/components/CameraCaptureComponent.jsx
@@ -1,23 +1,71 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const CameraCaptureComponent = () => {
-  const videoRef = useRef(null);
+  const [photoURL, setPhotoURL] = useState(null);
 
-  const handleCapture = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      videoRef.current.srcObject = stream;
-    } catch (error) {
-      console.error('Error accessing camera:', error);
-    }
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    handleStartCapture()
+  }, [])
+
+  const handleCapture = () => {
+    const context = canvasRef.current.getContext('2d');
+    context.drawImage(videoRef.current, 0, 0, 640, 480);
+    setPhotoURL(canvasRef.current.toDataURL('image/png'));
+    handleStopCapture();
   };
+
+  const handleStartCapture = async () => {
+    let constraints;
+    if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const backCamera = devices.find(device => device.kind === 'videoinput' && device.label.toLowerCase().includes('back'));
+      if (backCamera) {
+        constraints = { video: { deviceId: backCamera.deviceId }, audio: false };
+      } else {
+        constraints = { video: true, audio: false };
+      }
+    } else {
+      constraints = { video: true, audio: false };
+    }
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    videoRef.current.srcObject = stream;
+    videoRef.current.play();
+  };
+
+  const handleStopCapture = () => {
+    const stream = videoRef.current.srcObject;
+    const tracks = stream.getTracks();
+    tracks.forEach(track => track.stop());
+    videoRef.current.srcObject = null;
+  };
+
+  const handleNewCapture = () => {
+    setPhotoURL(null)
+    handleStartCapture()
+  }
+
+  const handleAccept = () => {
+    
+  }
 
   return (
     <div>
-      <video ref={videoRef} style={{ display: 'none' }} autoPlay></video>
-      <button onClick={handleCapture}>Ota kuva</button>
+      {(photoURL) ? 
+        <>
+          <img src={photoURL} alt="Otettu kuva" />
+          <button className='border-2 border-oamk-orange rounded-lg mx-2 my-1 px-3 py-1 ml-4 hover:scale-110 transition ease-in-out duration-300 hover:bg-oamk-orange' onClick={handleNewCapture}>Ota uusi kuva</button>
+          <button className='border-2 border-primary-blue rounded-lg mx-2 my-1 px-3 py-1 ml-4 hover:scale-110 transition ease-in-out duration-300 hover:bg-primary-blue hover:text-white' onClick={handleAccept}>Tallenna kuva</button>
+        </> :
+        <>
+          <video ref={videoRef} />
+          <canvas ref={canvasRef} width={640} height={480} style={{ display: 'none' }} />
+          <button className='border-2 border-primary-blue rounded-lg mx-2 my-1 px-3 py-1 ml-4 hover:scale-110 transition ease-in-out duration-300 hover:bg-primary-blue hover:text-white' onClick={handleCapture}>Ota kuva</button>
+        </>}
     </div>
-  );
-};
+  )
+}
 
 export default CameraCaptureComponent;

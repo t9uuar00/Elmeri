148ddbddfe09539e7b1ddf485591f4e8ccab2fbe
Markus <t9ojma01@students.oamk.fi>
2023-11-07 16:52:32 +0200

    raportinluonti

--- elmeri/src/App.js
+++ elmeri/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Etusivu from './components/Etusivu';
 import Tarkastuskohdat from './components/Tarkastuskohdat';
+import Raportinluonti from './Raportinluonti';
 
 function App() {
   return (
     <div className="App">
       <Etusivu></Etusivu>
+      <Raportinluonti />
       
       <Tarkastuskohdat></Tarkastuskohdat>
     </div>

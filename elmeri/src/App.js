75330b3cd1517cab320dedc0eccf7cf66aa736d3
Saana Pullola <t0pusa00@students.oamk.fi>
2023-11-09 20:31:37 +0200

    virheentarkastus duplikaattitiedostoille

--- elmeri/src/App.js
+++ elmeri/src/App.js
@@ -4,26 +4,9 @@ import RaporttiHistoria from "./components/RaporttiHistoria";
 import PdfUpload from "./components/PdfUpload";
 import Tarkastuskohdat from "./components/Tarkastuskohdat";
 
-//test
-import { collection, addDoc } from "firebase/firestore";
-import { firestoreDb } from "./firebase";
-
-//example, for testing firestore
-const addData = async () => {
-  try {
-    const docRef = await addDoc(collection(firestoreDb, "raports"), {
-      raport: "test",
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-};
-
 function App() {
   return (
     <div className="App">
-      <button onClick={addData}>Add data</button>
       <RaporttiHistoria />
       <Etusivu></Etusivu>
       <Tarkastuskohdat></Tarkastuskohdat>
--- elmeri/src/components/PdfUpload.jsx
+++ elmeri/src/components/PdfUpload.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import {
   firestoreDb,
   storageRef,
@@ -16,35 +15,74 @@ export default function PdfUpload() {
   let month = now.getMonth();
   let day = now.getDate();
   let date = `${day}.${month}.${year}`;
-  let fileName = `ElmeRRRIII_${date}`;
+  let fileName = `Elmeri_${date}`;
 
   const [selectedFile, setSelectedFile] = useState(null);
-  const [downloadUrl, setDownloadUrl] = useState(null);
 
-  //Tietyn raportin sijainti Firebase storagessa
+  // Tietyn raportin sijainti Firebase storagessa
   const raportRef = ref(storageRef, `/raports/${fileName}`);
   const raportsCollection = collection(firestoreDb, "raports");
 
-  //Hae ja valitse tiedosto
+  // Hae ja valitse tiedosto
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
 
-  //Lataa tiedosto Firebase storageen
-  const handleFileUpload = () => {
-    if (selectedFile) {
-      uploadBytes(raportRef, selectedFile).then((snapshot) => {
-        console.log("Tiedosto ladattu Firebase storageen!", snapshot);
-        //note to self, handle this better
-        getDownloadURL(raportRef).then((url) => {
-          saveFileMetadataToFirestore(url);
-        });
-      });
+  // Tarkista, jos tiedosto löytyy jo Firebase storagesta
+  const checkDuplicateFiles = async () => {
+    try {
+      // Onko kyseistä latauslinkkiä olemassa
+      const url = await getDownloadURL(raportRef);
+      console.log(
+        "Tiedosto on jo olemassa. Ei voida lisätä Firebase storageen."
+      );
+      return { exists: true, url };
+    } catch (error) {
+      if (error.code === "storage/object-not-found") {
+        console.log(
+          "Tiedostoa ei ole olemassa. Voidaan lisätä Firebase storageen."
+        );
+        return { exists: false, url: null };
+      } else {
+        console.log("Jotain meni pieleen. Error: ", error);
+        throw error;
+      }
     }
   };
 
-  //Lisää tiedoston metadata, mm. linkki tiedostoon
-  const saveFileMetadataToFirestore = (url) => {
+  // Lataa tiedosto Firebase storageen
+  const handleFileUploadToStorage = async () => {
+    if (selectedFile) {
+      // Tsekataan duplikaattien olemassaolo
+      try {
+        const result = await checkDuplicateFiles();
+        // Tiedosto on jo olemassa, estetään duplikaatin lataus
+        if (result.exists) {
+          console.log(
+            "Tiedosto on jo olemassa Firebase storagessa. Tiedostolinkki: ",
+            result.url
+          );
+        } else {
+          // Tiedostoa ei ole olemassa, ladataan tiedosto
+          const snapshot = await uploadBytes(raportRef, selectedFile);
+          console.log(
+            "Tiedosto ladattu onnistuneesti Firebase storageen!",
+            snapshot
+          );
+          const url = await getDownloadURL(raportRef);
+          saveFileMetadataToFirestore(url);
+        }
+      } catch (error) {
+        console.log(
+          "Tiedoston lataus Firebase storageen epäonnistui. Error: ",
+          error
+        );
+      }
+    }
+  };
+
+  // Lisää tiedoston metadata, mm. linkki tiedostoon
+  const saveFileMetadataToFirestore = async (url) => {
     const fileMetadata = {
       name: fileName,
       url: url,
@@ -52,27 +90,21 @@ export default function PdfUpload() {
       date_created: currentDate,
     };
 
-    //Lisätään ylläluotu objekti Firestoreen
-    addDoc(raportsCollection, fileMetadata)
-      .then((docRef) => {
-        const id = docRef.id;
-        console.log("File metadata saved to Firestore with ID: ", id);
-        return id;
-      })
-      .then((id) => {
-        //Päivitetään dokumentti ID myös omaksi fieldiksi dokumenttiin
-        setDoc(doc(firestoreDb, `raports/${id}`), { id }, { merge: true });
-        window.location.reload(); //Would be better as state transition refresh
-      })
-      .catch((error) => {
-        console.error("Error saving metadata: ", error);
-      });
+    try {
+      const docRef = await addDoc(raportsCollection, fileMetadata);
+      const id = docRef.id;
+      console.log("File metadata saved to Firestore with ID: ", id);
+      await setDoc(doc(firestoreDb, `raports/${id}`), { id }, { merge: true });
+      window.location.reload(); // Would be better as a state transition refresh
+    } catch (error) {
+      console.error("Error saving metadata: ", error);
+    }
   };
 
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
-      <button onClick={handleFileUpload}>Upload File</button>
+      <button onClick={handleFileUploadToStorage}>Upload File</button>
     </div>
   );
 }

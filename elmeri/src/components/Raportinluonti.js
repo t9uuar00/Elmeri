6f7bd85adb213d8ac7c0a896499b31d9e3d80805
Markus <t9ojma01@students.oamk.fi>
2023-11-27 06:46:56 +0200

    uutta

--- elmeri/src/components/Raportinluonti.js
+++ elmeri/src/components/Raportinluonti.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import './Raportinluonti.css'; // Import a CSS file for styling
 
 function Raportinluonti() {
   const [input1, setInput1] = useState('');
   const [input2, setInput2] = useState('');
   const [select1, setSelect1] = useState('Option 1');
   const [select2, setSelect2] = useState('Option A');
+  const [dropdownOptions1, setDropdownOptions1] = useState(['Nimi']);
+  const [dropdownOptions2, setDropdownOptions2] = useState(['Tila']);
+  const [progress, setProgress] = useState(0);
 
   const handleInputChange1 = (e) => {
     setInput1(e.target.value);
@@ -22,17 +26,31 @@ function Raportinluonti() {
     setSelect2(e.target.value);
   };
 
-  const handleAddClick = () => {
-    // lisää napin logic
+  const handleAddClick1 = () => {
+    setDropdownOptions1((prevOptions) => [...prevOptions, input1]);
+    setProgress((prevProgress) => prevProgress + 10);
   };
 
-  const handleSelectClick = () => {
-    // tähän logic
+  const handleAddClick2 = () => {
+    setDropdownOptions2((prevOptions) => [...prevOptions, input2]);
+    setProgress((prevProgress) => prevProgress + 10);
+  };
+
+  const handleClearClick1 = () => {
+    setDropdownOptions1((prevOptions) => prevOptions.slice(0, -1));
+    setProgress((prevProgress) => prevProgress - 10);
+  };
+
+  const handleClearClick2 = () => {
+    setDropdownOptions2((prevOptions) => prevOptions.slice(0, -1));
+    setProgress((prevProgress) => prevProgress - 10);
   };
 
   return (
-    <div className="container">
+    <div className="raportinluonti-container">
       <h1>Raportin Luonti</h1>
+      <label htmlFor="progress">Progress:</label>
+      <progress id="progress" value={progress} max="100" />
       <div className="input-with-dropdown">
         <div>
           <input
@@ -42,12 +60,15 @@ function Raportinluonti() {
             onChange={handleInputChange1}
             placeholder="Etunimi Sukunimi"
           />
-          <button onClick={handleAddClick}>Lisää</button>
+          <button onClick={handleAddClick1}>Lisää</button>
+          <button onClick={handleClearClick1}>Poista</button>
         </div>
         <select id="dropdown1" value={select1} onChange={handleSelectChange1}>
-          <option value="Option 1"> 1</option>
-          <option value="Option 2"> 2</option>
-          <option value="Option 3"> 3</option>
+          {dropdownOptions1.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
         </select>
       </div>
       <div className="input-with-dropdown">
@@ -59,12 +80,15 @@ function Raportinluonti() {
             onChange={handleInputChange2}
             placeholder="Valitse Tila"
           />
-          <button onClick={handleSelectClick}>Valitse</button>
+          <button onClick={handleAddClick2}>Lisää</button>
+          <button onClick={handleClearClick2}>Poista</button>
         </div>
         <select id="dropdown2" value={select2} onChange={handleSelectChange2}>
-          <option value="Option A"> A</option>
-          <option value="Option B"> B</option>
-          <option value="Option C"> C</option>
+          {dropdownOptions2.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
         </select>
       </div>
     </div>
